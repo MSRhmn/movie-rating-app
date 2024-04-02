dafdc5cf68c8e9f4563273e1f1d99b25780081ee
Md. Shamim Rahman <apubdtea@gmail.com>
2024-04-02 13:39:20 +0600

    feat: add json file loading capability

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
 from flask import Flask, request, jsonify
 import json
+
+app = Flask(__name__)
+
+# Load example data from JSON files.
+with open("./data/users.json") as f:
+    users = json.load(f)
+
+with open("./data/movies.json") as f:
+    movies = json.load(f)
+
+with open("./data/ratings.json") as f:
+    ratings = json.load(f)
+
+current_user = None
