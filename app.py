f64252488ddbeaf9b9a9fea76e15f24627bd3d61
Md. Shamim Rahman <apubdtea@gmail.com>
2024-04-02 15:44:08 +0600

    feat: add get_average_rating func

--- app.py
+++ app.py
@@ -14,3 +14,11 @@ with open("./data/ratings.json") as f:
     ratings = json.load(f)
 
 current_user = None
+
+# Define a helper function to calculate average rating for a movie.
+def get_average_rating(movie_id):
+    movie_ratings = [rating for rating in ratings if rating["movie_id"] == movie_id]
+    if movie_ratings:
+        return sum(rating["rating"] for rating in movie_ratings) / len(movie_ratings)
+    else:
+        return f"There is no ratings yet for movie id: `{movie_id}`."
