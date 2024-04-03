becf7fa1bcd49e08448c9552d26e0a21f9c5748a
Md. Shamim Rahman <apubdtea@gmail.com>
2024-04-03 22:57:32 +0600

    chore: import json data variables from data file

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 from flask import Flask, request, jsonify
 import json
+from data import users, movies, ratings
 
 app = Flask(__name__)
-
-# Load example data from JSON files.
-with open("./data/users.json") as f:
-    users = json.load(f)
-
-with open("./data/movies.json") as f:
-    movies = json.load(f)
-
-with open("./data/ratings.json") as f:
-    ratings = json.load(f)
 
 current_user = None
 
