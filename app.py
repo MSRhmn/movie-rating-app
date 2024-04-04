4752bf6745c74da544c84a46345aa94ba4a3ebad
Md. Shamim Rahman <apubdtea@gmail.com>
2024-04-04 16:44:58 +0600

    feat: import helper function
    import helper function from the helpers.py file and remove the existing
    code of the helper function from the app.py file.

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 from flask import Flask, request, jsonify
 import json
 from data import users, movies, ratings
+from helpers import get_average_rating
 
 app = Flask(__name__)
 
 current_user = None
-
-
-def get_average_rating(movie_id):
-    """Define a helper function to calculate average rating for a movie."""
-    movie_ratings = [rating for rating in ratings if rating["movie_id"] == movie_id]
-    if movie_ratings:
-        return sum(rating["rating"] for rating in movie_ratings) / len(movie_ratings)
-    else:
-        return f"There is no ratings yet for movie id: `{movie_id}`."
 
 
 @app.route("/login", methods=["POST"])
