7b69cc63b2974c07f1352ab2997546251599bed1
Md. Shamim Rahman <apubdtea@gmail.com>
2024-05-21 16:53:06 +0600

    chore: properly indent models file

--- movie_rating/api/models.py
+++ movie_rating/api/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     phone = models.CharField(max_length=15, blank=True, null=True)
+
 
 class Movie(models.Model):
     name = models.CharField(max_length=255)
     genre = models.CharField(max_length=100)
     rating = models.CharField(max_length=10)
     release_date = models.DateField()
+
 
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
