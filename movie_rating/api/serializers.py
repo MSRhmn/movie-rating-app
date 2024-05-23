2ceb3ef3b9aff4d536bf230d0a2ed3f06120e45b
Md. Shamim Rahman <apubdtea@gmail.com>
2024-05-23 11:54:45 +0600

    feat: add serializers to the app

---
+++ movie_rating/api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import User, Movie, Rating
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "phone", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            phone=validated_data["phone"],
+            password=validated_data["password"],
+        )
+        return user
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = "__all__"
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
+        fields = ["id", "user", "movie", "rating", "created_at"]
