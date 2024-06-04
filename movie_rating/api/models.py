598816af08e882da52bb69f84277959657ef2fe0
Md. Shamim Rahman <apubdtea@gmail.com>
2024-06-04 18:21:29 +0600

    fix: potential model bug

--- movie_rating/api/models.py
+++ movie_rating/api/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
+
 class User(AbstractUser):
     email = models.EmailField(unique=True)
 
-    groups = models.ManyToManyField(Group, related_name='api_user_set')
-    user_permissions = models.ManyToManyField(Permission, related_name='api_user_set')
+    groups = models.ManyToManyField(Group, related_name="api_user_set")
+    user_permissions = models.ManyToManyField(Permission, related_name="api_user_set")
+
 
 class Movie(models.Model):
     name = models.CharField(max_length=100)
@@ -16,9 +18,12 @@ class Movie(models.Model):
     def __str__(self):
         return self.name
 
+
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='movie_ratings')
+    movie = models.ForeignKey(
+        Movie, on_delete=models.CASCADE, related_name="movie_ratings"
+    )
     rating = models.FloatField()
 
     def __str__(self):
