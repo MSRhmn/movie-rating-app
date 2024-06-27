39cadfce9150ab9a6668ec29e545ae585bf2d275
Md. Shamim Rahman <apubdtea@gmail.com>
2024-06-27 18:51:12 +0600

    fix: optimize the user serializer class function

--- api/serializers.py
+++ api/serializers.py
@@ -10,11 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data["username"],
-            email=validated_data["email"],
-            password=validated_data["password"],
-        )
+        user = User.objects.create_user(**validated_data)
         return user
 
 
