54ab5779901fb79e4b1807d9a76f7448af8c1f96
Md. Shamim Rahman <apubdtea@gmail.com>
2024-09-23 10:36:24 +0600

    chore: indent settings appropriately

--- mr_project/settings.py
+++ mr_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # REST Framework apps
     "rest_framework",
-    'rest_framework.authtoken',
+    "rest_framework.authtoken",
 ]
 
 MIDDLEWARE = [
