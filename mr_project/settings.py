e369d5e8d741e79697902c44b16a8ed98672f4d4
Md. Shamim Rahman <apubdtea@gmail.com>
2024-09-29 17:27:27 +0600

    chore: indent project files

--- mr_project/settings.py
+++ mr_project/settings.py
@@ -133,10 +133,10 @@ AUTH_USER_MODEL = "api.User"
 
 # Authentication setup for DRF
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
