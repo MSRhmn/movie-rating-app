01a55b027237aad7cb294dde0b5460d10698957a
Md. Shamim Rahman <apubdtea@gmail.com>
2024-09-29 17:31:00 +0600

    fix: update the rating model field

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class User(AbstractUser):
 class Movie(models.Model):
     name = models.CharField(max_length=100)
     genre = models.CharField(max_length=50)
-    rating = models.FloatField()
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
     release_date = models.DateField()
 
     def __str__(self):
@@ -24,6 +24,7 @@ class Rating(models.Model):
         Movie, on_delete=models.CASCADE, related_name="movie_ratings"
     )
     rating = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.movie.name}: {self.rating}"
